f0263a3ed4b8e2dfd1cf9e67a90b0f7ad6faf471
bohdantverdyi <ownedd777@gmail.com>
2020-10-19 15:32:08 +0300

    Code beauty

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 resource "kubernetes_service" "service" {
   metadata {
-    name = var.app_name
-    namespace = var.app_namespace
+    name        = var.app_name
+    namespace   = var.app_namespace
     annotations = var.annotations
-    labels = local.labels
+    labels      = local.labels
   }
   spec {
-    selector = local.labels
-    type = var.type
-    load_balancer_ip = var.type == "LoadBalancer" ? var.load_balancer_ip : null
+    selector                    = local.labels
+    type                        = var.type
+    load_balancer_ip            = var.type == "LoadBalancer" ? var.load_balancer_ip : null
     load_balancer_source_ranges = var.type == "LoadBalancer" ? var.load_balancer_ips_whitelist : null
     dynamic "port" {
       iterator = port
       for_each = var.port_mapping
       content {
-        name = port.value.name
-        port = port.value.external_port
-        protocol = lookup(port.value, "protocol", "TCP")
-        node_port = lookup(port.value, "node_port", null)
+        name        = port.value.name
+        port        = port.value.external_port
+        protocol    = lookup(port.value, "protocol", "TCP")
+        node_port   = lookup(port.value, "node_port", null)
         target_port = port.value.internal_port
       }
     }
